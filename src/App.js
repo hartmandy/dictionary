1aff124fa6dbd69c01308b2d6ea922efe1a4d70f
hartmandy <120344775+hartmandy@users.noreply.github.com>
2023-07-18 10:27:14 -0400

    added search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
+import { useState } from "react";
+import Search from "./components/Search";
 
 function App() {
-  return "Hello World";
+  const [searchValue, setSearchValue] = useState("bibliophile");
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <div className="container mx-auto">
+      <div className="container max-w-xl mt-40">
+        <div className="rounded-xl border-slate-300 border-2 p-10 shadow-sm bg-slate-100 text-white tracking-wider">
+          <Search
+            onChange={handleChange}
+            searchValue={searchValue}
+            // onSubmit={fetchCurrentWeather}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
